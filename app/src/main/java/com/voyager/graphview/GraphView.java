e7a51b8f06d174887eb1293db04c2eada4c18e00
wuhaojie <w19961009@126.com>
2015-07-25 19:26:42 +0800

    Initial commit

---
+++ app/src/main/java/com/voyager/graphview/GraphView.java
@@ -0,0 +1,15 @@
+package com.voyager.graphview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 自定义控件——曲线视图
+ * Created by wuhaojie on 2015/7/25.
+ */
+public class GraphView extends View {
+    public GraphView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/com/voyager/graphview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.voyager.graphview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author wuhaojie
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
