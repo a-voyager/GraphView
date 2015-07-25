0e7a707c4d4fe3ae6516c697e801f117e21ba143
wuhaojie <w19961009@126.com>
2015-07-25 20:59:34 +0800

    初始化参数函数.

--- app/src/main/java/com/voyager/graphview/GraphView.java
+++ app/src/main/java/com/voyager/graphview/GraphView.java
@@ -14,6 +14,18 @@ import java.util.ArrayList;
 public class GraphView extends View {
 
     /**
+     * 因变量显示格式，如：23.5
+     */
+    private static final String Y_VALUE_FORMAT = "0.#";
+    /**
+     * 水平方向上与 View边界的间距，单位：dip
+     */
+    private static final int VER_MARGIN = 25;
+    /**
+     * 竖直方向上与 View边界的间距，单位：dip
+     */
+    private static final int HOS_MARGIN = 8;
+    /**
      * 上下文
      */
     private Context context;
@@ -21,6 +33,22 @@ public class GraphView extends View {
      * 坐标点集合
      */
     private ArrayList<GraphItem> items = null;
+    /**
+     * View的高度
+     */
+    private int height = 0;
+    /**
+     * View的宽度
+     */
+    private int width = 0;
+    /**
+     * 水平间距，单位：px
+     */
+    private int hosMargin;
+    /**
+     * 竖直间距，单位：px
+     */
+    private int verMargin;
 
     /**
      * 构造函数
@@ -36,6 +64,10 @@ public class GraphView extends View {
     private void initView(Context context, AttributeSet attrs, ArrayList<GraphItem> items) {
         this.context = context;
         this.items = items;
+        height = getHeight();
+        width = getWidth();
+        hosMargin = DensityUtils.dip2px(context, HOS_MARGIN);
+        verMargin = DensityUtils.dip2px(context, VER_MARGIN);
     }
 
     /**
