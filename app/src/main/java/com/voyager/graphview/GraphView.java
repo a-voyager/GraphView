61f984e7f8a56ce9ba0f812dcd47aa80274bd910
wuhaojie <w19961009@126.com>
2015-07-26 14:16:04 +0800

    完成线条绘制.

--- app/src/main/java/com/voyager/graphview/GraphView.java
+++ app/src/main/java/com/voyager/graphview/GraphView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,11 +21,11 @@ public class GraphView extends View {
      */
     private static final String Y_VALUE_FORMAT = "0.#";
     /**
-     * 水平方向上与 View边界的间距，单位：dip
+     * 竖直方向上与 View边界的间距，单位：dip
      */
     private static final int VER_MARGIN = 25;
     /**
-     * 竖直方向上与 View边界的间距，单位：dip
+     * 水平方向上与 View边界的间距，单位：dip
      */
     private static final int HOS_MARGIN = 8;
     /**
@@ -35,7 +36,16 @@ public class GraphView extends View {
      * 坐标轴宽度
      */
     private static final int AXIS_LINE_WIDTH = 2;
+    /**
+     * 自变量显示文字大小
+     */
     private static final int X_VALUE_TEXTSIZE = 12;
+    /**
+     * 因变量极差均分的份数
+     */
+    private static final float COPYS = 6.0f;
+    private static final int GRAPH_LINE_WIDTH = 3;
+    private static final String GRAPH_LINE_COLOR = "#7fffffff";
     /**
      * 上下文
      */
@@ -61,6 +71,7 @@ public class GraphView extends View {
      */
     private int verMargin;
     private Paint paint;
+    private ArrayList<Integer> xPoints;
 
     /**
      * 构造函数
@@ -105,20 +116,67 @@ public class GraphView extends View {
             return;
         }
         initView();
-        drawGraphLine(canvas);
+        drawGraphAxis(canvas);
         drawXValue(canvas);
+        drawYValue(canvas);
+    }
+
+    /**
+     * 绘制 y轴因变量
+     */
+    private void drawYValue(Canvas canvas) {
+        float yValue = 0;
+        float max = items.get(0).getyValue(), min = items.get(0).getyValue();     //因变量的最值
+        for (GraphItem item : items) {
+            yValue = item.getyValue();
+            max = yValue > max ? yValue : max;
+            min = yValue < min ? yValue : min;
+        }
+        float range = (max - min) > 0 ? (max - min) : COPYS;     //极差
+        max = max + range / COPYS;
+        min = min - range / COPYS;
+        Point[] points = getAllPoints(xPoints, max, min, height - 2 * verMargin - hosMargin, verMargin);
+        paint.setStrokeWidth(GRAPH_LINE_WIDTH);
+        paint.setColor(Color.parseColor(GRAPH_LINE_COLOR));
+        paint.setStyle(Paint.Style.STROKE);
+        for (int i = 0; i < points.length - 1; i++) {
+            canvas.drawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, paint);
+        }
+    }
+
+    /**
+     * 获取所有坐标点
+     *
+     * @param xPoints     自变量值集合
+     * @param max         因变量最大值
+     * @param min         因变量最小值
+     * @param graphHeight 图像高度范围
+     * @param verMargin   图像最高点
+     * @return 返回点阵集合
+     */
+    private Point[] getAllPoints(ArrayList<Integer> xPoints, float max, float min, int graphHeight, int verMargin) {
+        Point[] points = new Point[items.size()];
+        int yRealHeight;
+        for (int i = 0; i < items.size(); i++) {
+            yRealHeight = (int) (graphHeight + verMargin - graphHeight * (items.get(i).getyValue() - min) / (max - min));
+            points[i] = new Point(xPoints.get(i), yRealHeight);
+        }
+        return points;
     }
 
     /**
      * 绘制 x轴自变量
      */
     private void drawXValue(Canvas canvas) {
-        paint.setColor(Color.GRAY);
+        paint.setColor(Color.WHITE);
         paint.setStrokeWidth(0);
         paint.setTextSize(DensityUtils.dip2px(context, X_VALUE_TEXTSIZE));
+        paint.setTextAlign(Paint.Align.CENTER);
         int period = (width - 2 * hosMargin) / items.size(); //平均间隔
         int startX = hosMargin * 3;
+        xPoints = new ArrayList<>();
         for (GraphItem item : items) {
+            xPoints.add(startX);
             canvas.drawText(item.getxValue(), startX, verMargin / 2, paint);
             startX += period;
         }
@@ -127,12 +185,12 @@ public class GraphView extends View {
     /**
      * 绘制坐标轴
      */
-    private void drawGraphLine(Canvas canvas) {
+    private void drawGraphAxis(Canvas canvas) {
         paint.setColor(Color.parseColor(AXIS_LINE_COLOR));
         paint.setStrokeWidth(DensityUtils.dip2px(context, AXIS_LINE_WIDTH));
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawLine(hosMargin, verMargin, width - hosMargin, verMargin, paint);
-        int verUpMargin = height - hosMargin; //在竖直方向与View上端边界的间距
+        int verUpMargin = height - hosMargin - verMargin; //在竖直方向与View上端边界的间距
         canvas.drawLine(hosMargin, verUpMargin, width - hosMargin, verUpMargin, paint);
     }
 
